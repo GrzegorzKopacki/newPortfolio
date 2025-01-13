29eac7cc2ee626a1f75c4603336eb46c38de4ee3
Grzegorz <gkopacki92@gmail.com>
2025-01-13 23:46:22 +0100

    created structure for navbar

---
+++ newPortfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+export function Navbar() {
+	return (
+		<nav>
+			<a href="/">Portfolio</a>
+			<div>
+				<ul>
+					<li>
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a href="#experience">Experience</a>
+					</li>
+					<li>
+						<a href="#projects">Projects</a>
+					</li>
+					<li>
+						<a href="#contact">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	);
+}
