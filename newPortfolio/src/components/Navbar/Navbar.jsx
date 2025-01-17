8e49d00ce834c8fe859eccfacbac9893e17a2132
Grzegorz <gkopacki92@gmail.com>
2025-01-17 11:07:42 +0100

    added media for max width 830 px for navbar module

--- newPortfolio/src/components/Navbar/Navbar.jsx
+++ newPortfolio/src/components/Navbar/Navbar.jsx
@@ -9,6 +9,11 @@ export function Navbar() {
 				Portfolio
 			</a>
 			<div className={styles.menu}>
+				<img
+					className={styles.menuBtn}
+					src={getImageUrl("nav/menuIcon.png")}
+					alt="menu-button"
+				/>
 				<ul className={styles.menuItems}>
 					<li>
 						<a href="#about">About</a>
