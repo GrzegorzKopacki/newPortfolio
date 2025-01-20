35c89013895ee66e58ffccfcbb0b8b2ac30ce90b
Grzegorz <gkopacki92@gmail.com>
2025-01-20 23:27:32 +0100

    created new folder data

--- newPortfolio/src/components/Skills/Skills.jsx
+++ newPortfolio/src/components/Skills/Skills.jsx
@@ -1,6 +1,23 @@
 import styles from "./Skills.module.css";
+import {SKILLS} from "../../data/skills"
+
 
 export function Skills() {
-	return <section className={styles.container}></section>;
+	return (
+		<section id="experience" className={styles.container}>
+			<h2>Skills</h2>
+			<div>
+				<div>
+					{SKILLS.map((skill, id) => {
+						return (
+							<div key={id}>
+								<img src={skill.image} alt="" />
+							</div>
+						);
+					})}
+				</div>
+				<ul></ul>
+			</div>
+		</section>
+	);
 }
-
---
+++ newPortfolio/src/data/skills.js
@@ -0,0 +1,21 @@
+import ONE from "../../assets/skills/css.png";
+import TWO from "../../assets/skills/react.png";
+import THREE from "../../assets/skills/node.png";
+import FOUR from "../../assets/skills/graphql.png";
+import FIVE from "../../assets/skills/mongodb.png";
+import SIX from "../../assets/skills/figma.png";
+import ZERO from "../../assets/skills/html.png";
+
+export const SKILLS = [
+	{
+		id: 1,
+		title: "HTML",
+		image: ZERO,
+	},
+	{ id: 2, title: "CSS", image: ONE },
+	{ id: 3, title: "React", image: TWO },
+	{ id: 4, title: "Node", image: THREE },
+	{ id: 5, title: "GraphQL", image: FOUR },
+	{ id: 6, title: "MongoDB", image: FIVE },
+	{ id: 7, title: "Figma", image: SIX },
+];
