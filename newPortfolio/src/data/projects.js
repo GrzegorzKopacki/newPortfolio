d89558a32918e87cdfa3c9727393b2150bcf3803
Grzegorz <gkopacki92@gmail.com>
2025-01-26 23:53:35 +0100

    create new file projects.js

---
+++ newPortfolio/src/data/projects.js
@@ -0,0 +1,32 @@
+import ONE from "../../assets/projects/project.png"
+
+
+export const PROJECTS = [
+	{
+		title: "Project A",
+		image: ONE,
+		description:
+			"This is a project made to learn the latest languages by building an app.",
+		skills: ["React", "Express", "Node"],
+		demo: "https://www.example.com",
+		source: "https://www.github.com",
+	},
+	{
+		title: "Project B",
+		image: ONE,
+		description:
+			"This is a project made to learn the latest languages by building an app.",
+		skills: ["React", "Express", "Node", "Sass"],
+		demo: "https://www.example.com",
+		source: "https://www.github.com",
+	},
+	{
+		title: "Project C",
+		image: ONE,
+		description:
+			"This is a project made to learn the latest languages by building an app.",
+		skills: ["React", "Express", "Node"],
+		demo: "https://www.example.com",
+		source: "https://www.github.com",
+	},
+];
