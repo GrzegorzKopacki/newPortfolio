920ce7c6f30fdea934ce143eefc765143a2da588
Grzegorz <gkopacki92@gmail.com>
2025-01-27 00:00:22 +0100

    .

--- newPortfolio/src/components/Projects/Projects.jsx
+++ newPortfolio/src/components/Projects/Projects.jsx
@@ -10,6 +10,17 @@ export function Projects() {
 					return (
 						<div key={id}>
 							<img src={project.image} alt="project image" />
+							<h3>{project.title}</h3>
+							<p>{project.description}</p>
+							<ul>
+								{project.skills.map((skill, id) => {
+									<li key={id}>{skill}</li>;
+								})}
+							</ul>
+							<div>
+								<a href={project.demo}>Demo</a>
+								<a href={project.source}>Source</a>
+							</div>
 						</div>
 					);
 				})}
