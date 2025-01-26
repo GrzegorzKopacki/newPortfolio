30c322dd586e936f86a180a13dbee8749447220c
Grzegorz <gkopacki92@gmail.com>
2025-01-26 23:56:18 +0100

    .

--- newPortfolio/src/components/Projects/Projects.jsx
+++ newPortfolio/src/components/Projects/Projects.jsx
@@ -1,5 +1,19 @@
 import styles from "./Projects.module.css";
+import { PROJECTS } from "../../data/projects";
 
 export function Projects() {
-	return <section className={styles.container}></section>;
+	return (
+		<section className={styles.container}>
+			<h2>Projects</h2>
+			<div>
+				{PROJECTS.map((project, id) => {
+					return (
+						<div key={id}>
+							<img src={project.image} alt="project image" />
+						</div>
+					);
+				})}
+			</div>
+		</section>
+	);
 }
